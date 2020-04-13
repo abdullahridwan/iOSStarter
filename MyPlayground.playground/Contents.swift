122a3f49cb0d2f7acd52c8d8cd63f169ae172c16
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2020-04-12 23:58:16 -0400

    swift basic syntax playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+
+//comments
+
+
+
+/*variables are declared via var
+ Variable assignments can also change
+*/
+var str = "Hello, playground"
+print(str)
+str = "some data"
+print(str)
+
+
+//constants assignment cannot change
+let con = "more data"
+print(con)
+
+
+//implicit type declaration m
+var i = 23
+// i="string" wont work bc it implicitly relaizes that i is of type int
+
+//explicit type delcaration
+var j:String = "hello"
+
+
